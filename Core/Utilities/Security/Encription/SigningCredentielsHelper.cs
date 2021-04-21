c40713ab3220bc599834e5928aaa55a257e38872
KhayalT <teyyubovxeyal>
2021-04-21 22:07:37 +0400

    Encriptions and Json web tokens

---
+++ Core/Utilities/Security/Encription/SigningCredentielsHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encription
+{
+    public class SigningCredentielsHelper
+    {
+        public static SigningCredentials createSigningCredentials(SecurityKey securityKey)
+        {
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+        }
+
+    }
+}
